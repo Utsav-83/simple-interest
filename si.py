88d32bfd9a96421d980a4aad51d680a98b2d2742
Utsav-83 <utsav8391@gmail.com>
2025-10-04 09:28:31 +0530

    Update si.py

--- si.py
+++ si.py
@@ -4,5 +4,8 @@ rate = float(input("Enter the Rate of Interest (%): "))
 time = float(input("Enter the Time (in years): "))
 
 simple_interest = (principal * rate * time) / 100
+compound_interest = principal * (1 + rate/100) ** time - principal
 
 print("Simple Interest is:", simple_interest)
+print("Compound Interest is:", compound_interest)
+
